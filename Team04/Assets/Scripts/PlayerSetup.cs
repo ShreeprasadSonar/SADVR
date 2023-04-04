e33c6e60a0cb43bd2a39c414747726003785a6ed
Shreeprasad Sonar <65328555+ShreeprasadSonar@users.noreply.github.com>
2023-04-04 17:02:41 -0500

    Added Join Lobby with name menu screen, Added Connecting Screen, Fixed player bugs, addded script that makes text align to camera.

--- Team04/Assets/Scripts/PlayerSetup.cs
+++ Team04/Assets/Scripts/PlayerSetup.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using TMPro;
 
 public class PlayerSetup : MonoBehaviour
 {
     public CharacterMovement movement;
     public GameObject cardboardRig;
+    public string nickname;
+    public TextMeshPro nicknameText;
 
     public void IsLocalPlayer(){
         movement.enabled = true;
         cardboardRig.SetActive(true);
+    }
+
+    [PunRPC]
+    public void SetNickname(string _name){
+        nickname = _name;
+        nicknameText.text = nickname;
     }
 }
