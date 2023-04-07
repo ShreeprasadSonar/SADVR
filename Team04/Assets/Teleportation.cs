920defe5d40dfaefb6a0ee07b7e889d1b8418f32
mrabhishekchauhan <chauhan.abhishek596@gmail.com>
2023-04-06 23:05:24 -0500

    add teleport between levels

--- Team04/Assets/Teleportation.cs
+++ Team04/Assets/Teleportation.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Teleportation : MonoBehaviour
 {
     private GameObject player;
-    public GameObject TeleportationPoint;
+    public GameObject[] teleportDestinations;
+    private int currentDestinationIndex = 0;
 
     private void Update()
     {
@@ -11,18 +12,25 @@ public class Teleportation : MonoBehaviour
         {
             player = GameObject.FindGameObjectWithTag("Player");
             print("PLayer : " + player);
-            if (TeleportationPoint != null)
+            
+
+            if (teleportDestinations[currentDestinationIndex] != null)
             {
                 print(player.transform.position );
-                Vector3 position = TeleportationPoint.transform.position;
-                position.y += 4f; //Add an offset to avoid clipping through the ground
+                Vector3 position = teleportDestinations[currentDestinationIndex].transform.position;
+                if(currentDestinationIndex==1)
+                    currentDestinationIndex--;
+                else 
+                currentDestinationIndex++;
+                position.y += 1f; //Add an offset to avoid clipping through the ground
                 //position.x -= 2f;
                 ////position.y -= 4f;
                 //player.transform.position = position;
-                //Vector3 playerPosition =  player.transform.position;
-                //playerPosition.x= position.x;
-                //playerPosition.z = position.z;
-                player.transform.position = position;
+                Vector3 playerPosition =  player.transform.position;
+                playerPosition.x= position.x;
+                playerPosition.y= position.y;
+                playerPosition.z = position.z;
+                player.transform.position = playerPosition;
                 print(position);
                 print(player.transform.position);
                 
