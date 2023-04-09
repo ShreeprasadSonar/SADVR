04142dd1478d4c400040df749c93e04fe09eca1d
mrabhishekchauhan <chauhan.abhishek596@gmail.com>
2023-04-08 19:55:14 -0500

    added lift section with cues

--- Team04/Assets/Scripts/Teleportation.cs
+++ Team04/Assets/Scripts/Teleportation.cs
@@ -4,24 +4,14 @@ public class Teleportation : MonoBehaviour
 {
     private GameObject player;
     public GameObject[] teleportDestinations;
-    private int currentDestinationIndex = 1;
+    private bool isPointerOnLevel1=false;
+    private bool isPointerOnLevel2=false;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T)||Input.GetButtonDown("js0"))
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-            print("PLayer : " + player);
-            
-
-            if (teleportDestinations[currentDestinationIndex] != null)
+    private void Teleport(int index){
+        if (teleportDestinations[index] != null)
             {
                 print(player.transform.position );
-                Vector3 position = teleportDestinations[currentDestinationIndex].transform.position;
-                if(currentDestinationIndex==1)
-                    currentDestinationIndex--;
-                else 
-                currentDestinationIndex++;
+                Vector3 position = teleportDestinations[index].transform.position;
                 position.y += 1f; //Add an offset to avoid clipping through the ground
                 //position.x -= 2f;
                 ////position.y -= 4f;
@@ -35,7 +25,45 @@ public class Teleportation : MonoBehaviour
                 print(player.transform.position);
                 
             }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T)||Input.GetButtonDown("js0"))
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            print("PLayer : " + player);
+            if(isPointerOnLevel1)
+            {
+                Teleport(1);
+            }
+            if(isPointerOnLevel2)
+            {
+                Teleport(0);
+            }
+
+            
         }
+    }
+
+    public void OnPointerEnterLevel1()
+    {
+        isPointerOnLevel1 = true;
+    }
+
+    public void OnPointerExitLevel1()
+    {
+        isPointerOnLevel1 = false;
+    }
+
+    public void OnPointerEnterLevel2()
+    {
+        isPointerOnLevel2 = true;
+    }
+
+    public void OnPointerExitLevel2()
+    {
+        isPointerOnLevel2 = false;
     }
 
 }
