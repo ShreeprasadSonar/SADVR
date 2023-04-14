e305db41e3d425b3285c3b79fb699a37d557399f
vedangwartikar <vedangwartikar17@gmail.com>
2023-04-14 11:03:00 -0500

    Added circular loading bar for Pipe and Wire

---
+++ Team04/Assets/SimpleSpinner/SimpleSpinner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SimpleSpinner : MonoBehaviour
+{
+    public float speed = 50f; // rotation speed in degrees per second
+
+    private void Update()
+    {
+        transform.Rotate(0f, 0f, speed * Time.deltaTime); // rotate around the Z-axis
+    }
+}
