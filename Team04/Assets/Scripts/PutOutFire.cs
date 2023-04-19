34c21df26e97692c79746144945b0869ac166959
mrabhishekchauhan <chauhan.abhishek596@gmail.com>
2023-04-18 22:04:08 -0500

    added fire to lobby

--- Team04/Assets/Scripts/PutOutFire.cs
+++ Team04/Assets/Scripts/PutOutFire.cs
@@ -6,14 +6,17 @@ public class PutOutFire : MonoBehaviour
 {
     public GameObject Fire;
     public GameObject taskCompletedMsgScriptObj;
+    public GameObject progressBar;
 
     private float holdTime;
     private bool isPointerOnFire=false;
+    public AudioSource audiosource;
 
     void Update()
     {
         if ((Input.GetKey(KeyCode.E) || Input.GetButton("js2")) && isPointerOnFire) // Keyboard F, Android js2 (X)
         {
+            progressBar.SetActive(true);
             holdTime += Time.deltaTime;
 
             if (holdTime >= 3f)
@@ -21,10 +24,13 @@ public class PutOutFire : MonoBehaviour
 
                 taskCompletedMsgScriptObj.GetComponent<TaskCompletionMsg>().ShowTaskCompletedMessage();
                 Fire.SetActive(false);
+                progressBar.SetActive(false);
+                audiosource.Play();
             }
         }
         else
         {
+            progressBar.SetActive(false);
             holdTime = 0f;
         }
     }
