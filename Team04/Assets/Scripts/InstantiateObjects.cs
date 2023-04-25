a2d548f2d8740174be44632c1eb44efe937db74a
Shreeprasad Sonar <65328555+ShreeprasadSonar@users.noreply.github.com>
2023-04-24 20:41:14 -0500

    Update InstantiateObjects.cs

--- Team04/Assets/Scripts/InstantiateObjects.cs
+++ Team04/Assets/Scripts/InstantiateObjects.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class InstantiateObjects : MonoBehaviour
+public class InstantiateObjects : MonoBehaviourPunCallbacks
 {
     public GameObject plyer;
     // Start is called before the first frame update
